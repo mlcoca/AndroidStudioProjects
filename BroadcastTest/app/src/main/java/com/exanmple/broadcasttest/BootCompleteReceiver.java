11e8b42f2ee64743358f114bf66c5c6dd2a223bf
mlcoca <ma.coca.li@gmail.com>
2017-10-27 02:41:30 +0800

    10.27_0241

--- BroadcastTest/app/src/main/java/com/exanmple/broadcasttest/BootCompleteReceiver.java
+++ BroadcastTest/app/src/main/java/com/exanmple/broadcasttest/BootCompleteReceiver.java
@@ -3,6 +3,7 @@ package com.exanmple.broadcasttest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class BootCompleteReceiver extends BroadcastReceiver {
 
@@ -10,6 +11,6 @@ public class BootCompleteReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        Toast.makeText(context,"Boot Complete",Toast.LENGTH_LONG).show();
     }
 }
