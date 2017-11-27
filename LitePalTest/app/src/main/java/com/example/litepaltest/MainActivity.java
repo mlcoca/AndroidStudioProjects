5ffb42a2d431eba7d1ab7e9f31e415382a9a8f8d
mlcoca <ma.coca.li@gmail.com>
2017-11-27 23:21:36 +0800

    11272321

--- LitePalTest/app/src/main/java/com/example/litepaltest/MainActivity.java
+++ LitePalTest/app/src/main/java/com/example/litepaltest/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.litepaltest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import org.litepal.tablemanager.Connector;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button createDatabase = (Button)findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Connector.getDatabase();
+            }
+        });
     }
 }
